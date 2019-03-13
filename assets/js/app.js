78f02c4e28c3ee1a9770cd55689a5f78b5ccf2db
Tracy Le <tnle9@asu.edu>
2019-03-12 18:32:47 -0700

    Adding photos and new style

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// Sticky scroll nav
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
