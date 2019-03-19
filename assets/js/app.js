309cbd94233dfab1f3a55e2797bf3a3c415ee31e
Tracy Le <tnle9@asu.edu>
2019-03-18 20:38:16 -0700

    Updated layout and images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,24 @@
-// Sticky scroll nav
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+function EasyPeasyParallax() {
+scrollPos = $(this).scrollTop();
+$('.p1').css({
+  'background-position' : '50% ' + (-scrollPos/4)+"px"
+});
+$('.p2').css({
+  'background-position' : '50% ' + (-scrollPos/8)+"px"
+});
+$('.p3').css({
+  'background-position' : '70% ' + (-scrollPos/16)+"px"
+});
+$('.parallax-text').css({
+  'margin-top': (scrollPos/2)+"px",
+  'opacity': 1-(scrollPos/230)
+});
 }
+$(document).ready(function(){
+$(window).scroll(function() {
+  EasyPeasyParallax();
+});
+});
